6e2f85b18acb6bb2a4c19a3b843b98bb75df7eca
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-03-03 00:01:06 +0530

    c5

--- c5.php
+++ c5.php
@@ -1,1 +1,83 @@
-c
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>MOLESTATION</title>
+</head>
+
+<body bgcolor="#fef773">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr bgcolor="#343a40">
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Home</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Category</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">About Us</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Blogs</a></font>
+</tr>
+</table>
+<div class="wrapper">
+
+<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;"><font color="#FF0000">M</font>OLESTATION<br><font style="font-size:40%;"><u>[SECTION : 294,354-[A,B,C,D],355,509]</u></h1></font>
+</div>
+<table>
+<tr>
+<td><fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif" ><legend >
+<h1>As  per <font color="#FF0000">C</font>ONSTITUTION</font></h1></legend><img src="molestation.jpg" height="300" style="margin-top:5px;" align="right" border="3">
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 294 : </u></b></font><font size="+2" color="#000066">Any indecent or obscene act or singing, reciting, pronouncing of the lewd song done by the offender that causes ‘annoyance’ of others in public which cause mental nuisance will be considered molestation and the offender shall be punished with jail up to 3 months or fine or with both. </font><br><br>
+
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 354 : </u></b></font><font size="+2" color="#000066">This Section provides that whoever uses assault or force which is criminal in nature to a woman having an intention to outrage her modesty shall be punished with jail of at least one year which may extend to five years along with fine. <br><br>
+<dt>The essential ingredients of this section were laid down which are as follows:<br><br>
+<dd>1. The assault or the criminal force used must be on a woman i.e. the victim must be woman.<br><br>
+<dd>2. There must be an assault or a criminal force used.<br><br>
+<dd>3. There must be intention to outrage that woman’s modesty.<br></dd><br><br>
+Though this section specifically provides for the protection of women but it lacks in defining and providing a clear definition of the term ‘modesty’. The meaning of the same can be determined by various case laws as judiciary have contributed in defining the same.</dt>
+</font><br><br>
+
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 354-A : </u></b></font><font size="+2" color="#000066">Section 354-A further provides for sexual harassment and punishment for the same. It enumerates and identifies those acts which constitute the crime for sexual harassment. This section also provides for punishment for these offences individually which extends from one year to three years along with fine if necessary. </font><br><br>
+
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 354-B : </u></b></font><font size="+2" color="#000066"> Section 354-B provides for criminal acts to woman with an intention to disrobe the woman.<br><br>
+<dt>The acts which are considered as offence are:<br><br>
+<dd>I.Woman is assaulted.<br><br>
+<dd>II.There is a criminal assault.<br><br>
+<dd>III.Abetting any of the above acts.<br><br>
+<dd>VI.There is an intention to disrobe the woman.<br></dd><br>
+Punishments include imprisonment from one to three years along with fine.<br><br>
+ </font>
+ 
+ <font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 354-C : </u></b></font><font size="+2" color="#000066">Section 354-C provides for the criminal act of Voyeurism which means that being happy by seeing a scene. It provides that if any man captures any picture or an image of woman without her consent and further engaging in a private act constitutes an offence. Also, broadcasting the images is also an offence. Punishment for the same is jail of one to three years along with fine. </font><br><br>
+
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 354-D : </u></b></font><font size="+2" color="#000066"> 
+<dt>Section 354-D provides for the offence of stalking and under this section, two acts are considered a criminal act:<br><br>
+<dd>1.A woman who is followed by a stranger or a man without her consent and further tries to contact her is an offence.<br><br>
+<dd>2.Any act where a man follows and controls a woman’s life through mails or internet or through various technologies is an offence.<br></dd><br>
+The punishment provides that if a person committing the offence for the first time is imprisoned which may extend to three years along with fine. Any person committing the same for the second time may be punished for a term of imprisonment which may extend till five years along with fine.
+ </font><br><br>
+ 
+ <font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 355 : </u></b></font><font size="+2" color="#000066">It deals with the with an intent to dishonour the person. Punishment will be imprisonment which may extend to two years along with fine.</font><br><br>
+ 
+ <font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 509 : </u></b></font><font size="+2" color="#000066">Whoever, intending to insult the modesty of any woman, utters any word, makes any sound or gesture, or exhibits any object, intending that such word or sound shall be heard, or that such gesture or object shall be seen, by such woman, or intrudes upon the privacy of such woman, shall be punished with simple imprisonment for a term which may extend to three years, and also with fine.</font><br><br>
+</fieldset>
+
+</tr>
+</table>
+<br>
+<fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif"><legend><h1><font color="#FF0000">E</font>XPLANATION </font></h1></legend>
+
+<div style="background-color:#CCC; margin-left:5px; margin-right:5px;"><br>
+<dl>
+<dt>
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"  size="+3" ><u><b>Explanation 1</u></b></dt><font>
+<dd>
+<font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">Does any obscene act in any public place.</font></dd><br>
+
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"  size="+3" ><u><b>Explanation 2</u></b></dt><font>
+<dd>
+<font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">Sings, recites or utters any obscene song, ballad or words, in or near any public place, shall be punished with imprisonment of either description for a term which may extend to three months, or with fine, or with both.</font></dd><br>
+
+
+</div>
+</fieldset>
+
+</body>
+</html>
+
