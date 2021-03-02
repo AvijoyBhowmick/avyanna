f31ce6aea8596ca99e265b9341ab0594fbaa5e1e
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-03-03 00:01:32 +0530

    c6

---
+++ c6.php
@@ -0,0 +1,44 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SEXUAL HARRASMENT</title>
+</head>
+
+<body bgcolor="#fef773">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr bgcolor="#343a40">
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Home</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Category</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">About Us</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Blogs</a></font>
+</tr>
+</table>
+<div class="wrapper">
+
+<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;"><font color="#FF0000">S</font>EXUAL <font color="#FF0000">H</font>ARASSMENT<br><font style="font-size:40%;"><u>[SECTION : 354 A]</u></h1></font>
+</div>
+<table>
+<tr>
+<td><fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif" ><legend >
+<h1>As  per <font color="#FF0000">C</font>ONSTITUTION</font></h1></legend><img src="sexual harasment.jpeg" height="300" style="margin-top:5px;" align="right" border="3">
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 354 A talks about: </u></b></font><font size="+2" color="#000066">A man committing any of the following acts—<br>
+[i]Physical contact and advances involving unwelcome and explicit sexual overtures.<br>
+[ii]A demand or request for sexual favours.<br>
+[iii]Showing pornography against the will of a woman.<br>
+[iv]Making sexually coloured remarks, shall be guilty of the offence of sexual harassment.
+ </font><br>
+
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" font size="+3"><u><b>Punishment: </u></b></font><br> <font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">Any man who commits the offence specified in clause (i) or clause (ii) or clause (iii) shall be punished with rigorous imprisonment for a term which may extend to three years, or with fine, or with both.<br>
+Any man who commits the offence specified in clause (iv) shall be punished with imprisonment of either description for a term which may extend to one year, or with fine, or with both.</font>
+
+</fieldset>
+
+</tr>
+</table>
+
+
+</body>
+</html>
+
