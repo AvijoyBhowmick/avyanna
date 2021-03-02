d0988c39a1eb96f55f56ad5467e712a4ebb31eb1
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-03-03 00:00:33 +0530

    c4

---
+++ c4.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>TORTURE</title>
+</head>
+
+<body bgcolor="#fef773">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr bgcolor="#343a40">
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Home</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Category</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">About Us</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Blogs</a></font>
+</tr>
+</table>
+<div class="wrapper">
+
+<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;"><font color="#FF0000">T</font>ORTURE<br><font style="font-size:40%;"><u>[SECTION : 498 A]</u></h1></font>
+</div>
+<table>
+<tr>
+<td><fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif" ><legend >
+<h1>As  per  <font color="#FF0000">C</font>ONSTITUTION</font></h1></legend><img src="torture_girl.jpg" height="300" style="margin-top:5px;" align="right" border="3">
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 498 A talks about: </u></b></font><font size="+2" color="#000066">Both mental and physical.Husband or relative of husband of a woman subjecting her to cruelty.Whoever, being the husband or the relative of the husband of a woman, subjects such woman to cruelty shall be pun­ished with imprisonment for a term which may extend to three years and shall also be liable to fine.</font><br>
+
+
+</fieldset>
+
+</tr>
+</table>
+<br>
+<fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif"><legend><h1><font color="#FF0000">E</font>XPLANATION  </font></h1></legend>
+
+<div style="background-color:#CCC; margin-left:5px; margin-right:5px;"><br>
+<dl>
+<dt>
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"  size="+3" ><u><b>Explanation 1</u></b></dt><font>
+<dd>
+<font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">Any wilful conduct which is of such a nature as is likely to drive the woman to commit suicide or to cause grave injury or danger to life, limb or health (whether mental or physical) of the woman.</font></dd><br>
+
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"  size="+3" ><u><b>Explanation 2</u></b></dt><font>
+<dd>
+<font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">Harassment of the woman where such harassment is with a view to coercing her or any person related to her to meet any unlawful demand for any property or valuable security or is on account of failure by her or any person related to her to meet such demand.</font></dd><br>
+
+
+</div>
+</fieldset>
+
+</body>
+</html>
+
