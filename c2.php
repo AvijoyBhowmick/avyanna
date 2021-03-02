064100374d66d571abf49143fb6ececc07a43720
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-03-02 23:59:25 +0530

    c2

---
+++ c2.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>KIDNAPPING & ABDUCTION</title>
+</head>
+
+<body bgcolor="#fef773">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr bgcolor="#343a40">
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Home</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Category</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">About Us</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Blogs</a></font>
+</tr>
+</table>
+<div class="wrapper">
+
+<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;"><font color="#FF0000">K</font>IDNAPPING & <font color="#FF0000">A</font>BDUCTION</font><br><font style="font-size:40%;"><u>[SECTION : 359 - 373]</u></font></h1>
+
+</div>
+<table>
+<tr>
+<td><fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif" ><legend >
+<h1>As  per  <font color="#FF0000">C</font>ONSTITUTION</font></h1></legend><img src="abduction.jpg" height="300" style="margin-top:5px;" align="right" border="3">
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>Kidnapping and Abduction for a different purpose(section 363 to 373): as per section 363 </u></b></font><font size="+2" color="#000066">Talks about punishments for kidnapping Whoever kidnaps any person from India or from lawful guardianship, shall be punished with imprisonment of either description for a term which may extend to seven years, and shall also be liable to fine. In Chandrakala v. Vipin Menon.The supreme court declined to convict the father, who was accused of kidnapping his minor daughter who was living with her maternal grand father due to strained relationship between her parents, on the ground that the accused was the natural guardian of the child.<br></font><br>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><u><b>As per Section 359: </u></b></font><font size="+2" color="#000066">Defined as kidnapping, Kidnapping is of two types: kidnapping from India, and kidnapping from lawful guardianship.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><u><b>As per section 360 talks about kidnapping from India and section 361 talks about Kidnapping from lawful guardianship: </u></b></font><font size="+2" color="#000066"> In State of Haryana v. Raja Ram,in this case the prosecutrix was a young girl of 14 years. She became friendly with a person called Jai Narain, aged 32, who was a frequent visitor. When Jai Narain was forbidden by prosecutrix’s father from coming home, he sent a message through a Raja Ram. She was constantly persuaded to leave the house and come with Jai Narain, who would keep her in a lot of material comforts. One night, the prosecutrix arranged to meet Jai Narain in his house and went to meet him where she was seduced by Jai Narain.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><u><b>As per Section 364 talks about punishment:  </u></b></font><font size="+2" color="#000066">It means this section provides that Kidnapping or abducting in order to murder, it means Whoever Kidnaps or abducts any person in order that such person may be murdered or may be so disposed of as to be put in danger of being murdered, shall be punished with imprisonment for life or rigorous imprisonment for a term which may extend to ten years, and shall also be liable to fine.</font><br><br>
+</dl>
+
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" font size="+3"><u><b>Illustration:</u></b></font><br> <font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">'A' kidnaps 'B' from India, intending or knowing it to be likely that 'B' may be sacrificed to an Idol. 'A' has committed an offense in this section.
+<br>So, this section applies if a person has been abducted with the intention that he be murdered. (In Upendra Nath v. Emperor.) The actual murder of the person is not required. It is sufficient that there was abduction with intent to murder.</font><br><br></dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per 364A Kidnapping for ransom, etc:  </u></b></font><font size="+2" color="#000066">Whoever kidnaps or abducts any person or keeps a person in detention after such kidnapping or abduction and threatens to cause death or hurt to such person, or by his conduct gives rise to a reasonable apprehension that such person may be put to death or hurt, or causes hurt or death to such person in order to compel the Government or  any foreign State or international inter-governmental organization or any other person to do or abstain from doing any act or to pay a ransom, shall be punishable with death, or imprisonment for life, and shall also be liable to fine.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per section 365 Kidnapping or abducting with intent secretly and wrongfully to confine person:  </u></b></font><font size="+2" color="#000066">Whoever kidnaps or abducts any person with intent to cause that person to be secretly and wrongfully confined, shall be punished with imprisonment of either description for a term which may extend to seven years, and shall also be liable to fine.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per section 366 kidnapping, abducting or compelling a woman to marry, etc:  </u></b></font><font size="+2" color="#000066">Whoever kidnaps or abducts any woman with intent that she may be compelled, or knowing it to be likely that she will be compelled, to marry any person against her will, or in order that she may be forced or seduced to illicit intercourse, or knowing it to be likely that she will be forced or seduced to illicit intercourse shall be punished with imprisonment of either description for a term which may extend to ten years, and shall also be liable to fine; 1[and whoever, by means of criminal intimidation as defined in this Code or of abuse of authority or any other method of compulsion, induces any woman to go from any place with intent that she may be, or knowing that it is likely she will be, forced or seduced to illicit intercourse with another person shall be punished as aforesaid.
+
+</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per Section 366A procreation of minor girl:  </u></b></font><font size="+2" color="#000066">Whoever, by any means whatsoever, induces any minor girl under the age of eighteen years to go from any place or to do any act with intent that such a girl may be, or knowing that it is likely that she will be, forced or seduced to illicit intercourse with another person shall be punishable with imprisonment which may extend to ten years, and shall also be liable to fine.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per Section 366B Importation of girl from foreign country:  </u></b></font><font size="+2" color="#000066">Whoever imports into[India] from any country outside India or from the [State of Jammu and Kashmir] any girl under the age of twenty-one years with intent that she may be, or knowing it to be likely that she will be, forced or seduced to illicit intercourse with another person, shall be punishable with imprisonment which may extend to ten years and shall also be liable to fine.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per Section 367 Kidnapping or abducting in order to subject person to grievous hurt, slavery, etc:  </u></b></font><font size="+2" color="#000066">Whoever kidnaps or abducts any person in order that such person may be subjected, or may be so disposed of as to be put in danger of being subject to grievous hurt, or slavery, or to unnatural lust of any person, or knowing it to be likely that such person will be so subjected or disposed of, shall be punished with imprisonment of either description for a term which may extend to ten years, and shall also be liable to fine.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per Section 368 Wrongfully concealing or keeping in confinement, kidnapped or abducted person:  </u></b></font><font size="+2" color="#000066">Whoever, knowing that any person has been kidnapped or has been abducted, wrongfully conceals or confines such person, shall be punished in the same manner as if he had kidnapped or abducted such person with the same intention or knowledge, or for the same purpose as that with or for which he conceals or detains such person in confinement</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per Section 369 Kidnapping or abducting child under ten years with intent to steal from its person:  </u></b></font><font size="+2" color="#000066">Whoever kidnaps or abducts any child under the age of ten years with the intention of taking dishonestly any movable property from the person of such child, shall be punished with imprisonment of either description for a term which may extend to seven years, and shall also be liable to fine.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per Section 370 Buying or disposing of any person as slaves:  </u></b></font><font size="+2" color="#000066">Whoever imports, export, removes, buys, sells or disposes of any person as a slave, or accepts, receives or detains against his will any person as slave, shall be punished with imprisonment of either description for a term which may extend to seven years and shall also be liable to fine.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per Section 371 talks about Habitual dealing in slaves:  </u></b></font><font size="+2" color="#000066">Whoever habitually imports, exports, removes, buys, sells, traffics or deals in slaves, shall be punished with [imprisonment for life] or with imprisonment of either description for a term not exceeding the years, and shall also be liable to fine.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per Section 372 selling minor for purpose of prostitution, etc:  </u></b></font><font size="+2" color="#000066">Whoever sells, lets to hire, or otherwise disposes of any person under the age of eighteen years with intent that such person shall at any age be employed or used for the purpose of prostitution or illicit intercourse with any person or for any unlawful and immoral purpose, or knowing it to be likely that such person will at any age be] employed or used for any such purpose, shall be punished with imprisonment of either description for a term which may extend to ten years, and shall be liable to fine.</font><br><br>
+</dl>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" style="al"><u><b>As per Section 373 talks about Buying minor for the purpose of prostitution etc:  </u></b></font><font size="+2" color="#000066">Whoever buys, hires or otherwise obtains possession of any 1[person under the age of eighteen years with intent that such person shall at any age be employed or used for the purpose of prostitution or illicit intercourse with any person or for any unlawful and immoral purpose, or knowing it to be likely that such person will at any age be] employed or used for any purpose, shall be punished with imprisonment of either description for a term which may extend to ten years, and shall also be liable to fine.</font><br><br>
+</dl>
+
+
+</fieldset>
+
+</tr>
+</table>
+<br>
+<fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif"><legend><h1><font color="#FF0000">E</font>XPLANATION </font></h1></legend>
+
+<div style="background-color:#CCC; margin-left:5px; margin-right:5px;"><br>
+<dl>
+<dt>
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"  size="+3" ><u><b>Explanation 1</u></b></dt><font>
+<dd>
+<font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">When a female under the age of eighteen years sold, let for hire, or otherwise disposed of to a prostitute or to any person who keeps or manages a brothel, the person so disposing of such female shall, until the contrary is proved, be presumed to have disposed of her with the intent that she shall be used for the purpose of prostitution.</font></dd><br>
+
+<dt><font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3" ><u><b> Explanation 2</u></b></font></dt>
+<dd><font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">For the purposes of this section “illicit intercourse” means sexual intercourse between persons not united by marriage or by any union or tie which, though not amounting to a marriage, is recognized by the personal law or custom of the community to which they belong or, where they belong to different communities, of both such communities, as constituting between them a quasi-marital relation.</font></dd><br>
+
+<dt><font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3" ><u><b> Explanation 3</u></b></font></dt>
+<dd><font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">Any prostitute or any person keeping or manag­ing a brothel, who buys, hires or otherwise obtains possession of a female under the age of eighteen years shall, until the con­trary is proved, be presumed to have obtained possession of such female with the intent that she shall be used for the purpose of prostitution.</font></dd><br><br>
+</dl>
+
+</div>
+</fieldset>
+
+</body>
+</html>
+
