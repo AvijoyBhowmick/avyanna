c4a66dad24f6ec2de40793822fe0cf173ea5ec9c
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-03-03 00:00:06 +0530

    c3

---
+++ c3.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>DOWRY</title>
+</head>
+
+<body bgcolor="#fef773">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr bgcolor="#343a40">
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Home</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Category</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">About Us</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Blogs</a></font>
+</tr>
+</table>
+<div class="wrapper">
+
+<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;"><font color="#FF0000">D</font>OWRY</font><br><font style="font-size:40%;"><u>[SECTION : 302 - 304 B]</u></h1></font>
+</div>
+<table>
+<tr>
+<td><fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif" ><legend >
+<h1>As  per <font color="#FF0000">C</font>ONSTITUTION</h1></legend><img src="dowry_car.jpg" height="300" style="margin-top:5px;" align="right" border="3">
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 304 B: </u></b></font><font size="+2" color="#000066">Dowry death- Where the death of a woman is caused by any burns or bodily injury or occurs otherwise than under normal circumstances within seven years of her marriage and it is shown that soon before her death she was subjected to cruelty or har­assment by her husband or any relative of her husband for, or in connection with, any demand for dowry, such death shall be called <u>"dowry death"</u>, and such husband or relative shall be deemed to have caused her death.</font><br>
+
+<font size="+3" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><u><b>As per section 302 talks about punishment for murder: </u></b></font><font size="+2" color="#000066">It means Whoever commits murder shall be punished with death, or [imprisonment for life] and shall also be liable to fine.</font><br><br>
+</dl>
+
+</fieldset>
+
+</tr>
+</table>
+<br>
+<fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif"><legend><h1><font color="#FF0000">E</font>XPLANATION </font></h1></legend>
+
+<div style="background-color:#CCC; margin-left:5px; margin-right:5px;"><br>
+<dl>
+<dt>
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"  size="+3" ><u><b>Explanation</u></b></dt><font>
+<dd>
+<font size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" color="#000066">For the purpose of this subsection, "dowry" shall have the same meaning as in section 2 of the Dowry Prohibition Act, 1961 (28 of 1961).Whoever commits dowry death shall be punished with imprison­ment for a term which shall not be less than seven years but which may extend to imprisonment for life.</font></dd><br>
+
+
+
+</div>
+</fieldset>
+
+</body>
+</html>
