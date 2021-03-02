fd2a3c7c52265ba7caccf74ede31a7f5f32d9351
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-03-03 00:02:26 +0530

    c8

---
+++ c8.php
@@ -0,0 +1,44 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SPECIAL LAWS</title>
+</head>
+
+<body bgcolor="#fef773">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr bgcolor="#343a40">
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Home</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Category</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">About Us</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Blogs</a></font>
+</tr>
+</table>
+<div class="wrapper">
+
+<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;"><font color="#FF0000">S</font>PECIAL <font color="#FF0000">L</font>AWS</font><br></font></h1></font>
+</div>
+<table>
+<tr>
+<td><fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif" ><legend >
+<h1>As  per <font color="#FF0000">C</font>ONSTITUTION</font></h1></legend><img src="CIUSL.jpg" height="300" style="margin-top:5px;" align="right" border="3">
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>The crimes identified under the special laws: </u></b></font><font size="+2" color="#000066">Although all laws are not gender-specific, the provisions of law affecting women significantly have been reviewed periodically and amendments carried out to keep pace with the emerging requirements. Some acts which have special provisions to safeguard women and their interests are:<br><br>
+1.The employees' Insurance Act,1948<br>
+2.The Plantation Labour Act, 1951<br>
+3.Special Marriage Act, 1954<br>
+4.The Hindu Marriage Act, 1955<br>
+5.Dowry Prohibition Act, 1961<br>
+6.The Equal Remuneration Act, 1976<br>
+7.The child marriage Restraint(Amendment) Act, 1979<br>
+8.Commission of Sati(Prevention) Act, 1987<br>
+9.Hindu Succession Act,1956.
+ </font><br>
+
+
+
+
+</body>
+</html>
+
+
