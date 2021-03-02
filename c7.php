83dc4a28e0635caef4e6b12613b6ddad01fe3421
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-03-03 00:01:58 +0530

    c7

---
+++ c7.php
@@ -0,0 +1,38 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>IMPORTATION</title>
+</head>
+
+<body bgcolor="#fef773">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr bgcolor="#343a40">
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Home</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Category</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">About Us</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Blogs</a></font>
+</tr>
+</table>
+<div class="wrapper">
+
+<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;"><font color="#FF0000">I</font>MPORTATION<br></font><font style="font-size:40%;"><u>[SECTION : 366 B]</u></h1></font>
+</div>
+<table>
+<tr>
+<td><fieldset style="border-color:#333; font-size:30px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif" ><legend >
+<h1>As  per  <font color="#FF0000">C</font>ONSTITUTION</font></h1></legend><img src="importation of g.jpeg" height="300" style="margin-top:5px;" align="right" border="3">
+<font  face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+3"><u><b>As per Section 366B Importation of girl from foreign country: </u></b></font><font size="+2" color="#000066">Whoever imports into India from any country outside India or from the State of Jammu and Kashmir any girl under the age of twenty-one years with intent that she may be, or knowing it to be likely that she will be, forced or seduced to illicit intercourse with another person, shall be punishable with imprisonment which may extend to ten years, and shall also be liable to fine.
+ </font><br>
+
+
+
+</fieldset>
+
+</tr>
+</table>
+
+</body>
+</html>
+
