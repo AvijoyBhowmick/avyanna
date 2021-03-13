50f0253af9f36b1122f970335b1a0584456b0cd3
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-03-14 00:08:30 +0530

    safety tips

---
+++ safety_tips.php
@@ -0,0 +1,195 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Safety Tips</title>
+</head>
+
+<body bgcolor="#fef773">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr bgcolor="#343a40">
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Home</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Category</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">About Us</a></font>
+<th><font color="#999999" size="+2" face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif"><a href="inbook 01.html" style="color:#CCC; text-decoration:none;">Blogs</a></font>
+</tr>
+</table>
+<div class="wrapper">
+
+<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;"><font color="#FF0000">SAFETY TIPS</font></h1></center>
+<br>
+<fieldset  style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+Be aware of your surroundings.Don’t let your guard down.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >> If things seem even a slight unsafe get out of that place immediately.
+ </font>
+</fieldset><br><br>
+
+<fieldset style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+Please trust and make good use of your ‘gut feel’ or ‘intuition’ or ‘sixth sense’ in each and every place and situation.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >> There will invariably be signs, however small or seemingly insignificant, that tell or alarm you that all is just not OK. You may not understand it or be able to analyze it consciously, but please do NOT ignore it. It is your personal threat alarm beeping.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>  Make it a habit to read and notice these warning signs and go with your intuition to quickly retract yourself, move out or immediately get away from potentially unsafe situations.
+ </font>
+</fieldset><br><br>
+
+<fieldset style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+Don’t take eve teasing lightly.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>  Respond with a stern voice.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>   Threaten to take a picture which might scare them away.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>     If the eve teasing still persists then immediately raise your voice to gather a crowd.
+ </font>
+</fieldset><br><br>
+
+<fieldset style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+As much as possible avoid late night travel using public transport.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>  If avoiding is not possible then be sure to travel only on crowded bus.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>   Avoid taking road side cabs.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>    Avoid using a bus which has no passenger or few passenger.
+ </font>
+</fieldset><br><br>
+
+<fieldset  style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+While using 2 wheeler be sure to wear helmet at all times (especially at night). Don’t stop for any stranger.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >> In case attacked, use your helmet to defend yourself.
+ </font>
+</fieldset><br><br>
+
+<fieldset style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+ While driving a car.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>  Avoid parking at desolate area.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>   Look inside the car especially the back seat before unlocking and entering.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>      Lock the doors immediately after entering the car.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>     Start the car and drive off rather then call someone. Calls can wait.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>    Don’t give lift to strangers.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>    Don’t stop your car for strangers, especially at night.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>     Take known routes, avoid short cuts which you don’t know.
+ </font>
+</fieldset><br><br>
+
+<fieldset style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+  While travelling at night don’t keep your phone in your hand.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>   Remember phone is the first thing your attacker will throw away.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>    Activate your speed dials which can be used to dial numbers in emergency.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>       Keep it in your pocket or at a place from where you can easily take it out and dial a number and leave it on.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>     Once the number has been dialled start shouting and try to speak out your location so that the other person can inform police about your location.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>     If you have a smart phone then install emergency apps which can send sms to known people and can even dial police no. in case of emergency.
+ </font>
+</fieldset><br><br>
+
+<fieldset  style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+After getting dropped don’t stay outside to take a call.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >> After getting dropped don’t stay outside to take a call.
+ </font>
+</fieldset><br><br>
+
+<fieldset  style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >If you feel that someone is following you immediately rush to a crowded place and if need to be don’t be embarrassed to shout and gain attention.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>   Don’t be embarrassed to share such things with your family and friends.
+ </font>
+</fieldset><br><br>
+
+<fieldset style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+  Following are the most vulnerable areas to attack.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>   Use your knees to hit hard between the legs of the attacker.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>      Using your fingers to poke hard in the eyes.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>         Hitting front part of the neck on the Adams apple will leave him in a shock.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>      Punching in the stomach will leave the attacker breathless and unable to chase you.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>      Hitting hard in the chest area.
+ </font>
+</fieldset><br><br>
+
+
+<fieldset style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+  In case you feel you are being stalked or regularly followed, don’t keep it to yourself because what might be on lighter side now might become dangerous if not controlled.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>  Seek help of friends and family.
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#000;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>    If need be approach police immediately.
+ </font>
+</fieldset><br><br>
+
+
+<fieldset style=" font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:30px; font-weight:bolder;" >
+ Important Contact Number.</fieldset><br>
+<fieldset style="border-color:#333; background-color:#F00;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>  Women Helpline ( All India ) : 1091
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#F00;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>  Women Helpline Domestic Abuse : 181
+ </font>
+</fieldset><br>
+<fieldset style="border-color:#333; background-color:#F00;">
+<font face="Gotham, Helvetica Neue, Helvetica, Arial, sans-serif" size="+2" color="#fef773"> >>  Police : 100
+ </font>
+</fieldset><br>
+</body>
+</html>
