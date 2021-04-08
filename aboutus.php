aec5d590c89b916287902e7033a1f3efbe5a5206
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-04-08 22:19:41 +0530

    updated aboutus

--- aboutus.php
+++ aboutus.php
@@ -3,117 +3,44 @@
 <head>
 <meta charset="utf-8">
 <title>About Us</title>
-<style>
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,5,0,5);
-}
-
-/* Float four columns side by side */
-.column {
-  float: left;
-  width: 20%;
-  padding: 0 10px;
-}
-
-/* Remove extra left and right margins, due to padding */
-.row {margin: 0 -5px;}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Responsive columns */
-@media screen and (max-width: 600px) {
-  .column {
-    width: 100%;
-    display: block;
-    margin-bottom: 20px;
-  }
-}
-
-/* Style the counter cards */
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  padding: 16px;
-  text-align: center;
-  background-color: #f1f1f1;
-}
-</style>
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="stylesheet" href="AUS.css">
 </head>
 
-<body bgcolor="#fef773">
-<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;">ABOUT US</h1>
+<body>
+<section>
+<div class="color"></div>
+<div class="color"></div>
+<div class="color"></div>
+<div class="color"></div>
+<div class="box">
+<div class="square" style="--i:1;"></div>
+<div class="square" style="--i:2;"></div>
+<div class="square" style="--i:3;"></div>
+<div class="square" style="--i:4;"></div>
+<div class="box1">
+<div class="square1"></div>
+<div class="container">
+	<div class="card">
+    	
+    	<div class="imgBx">
+        	<img src="newfinallogo.png"></img>
+        </div>
+    	<div class="content">
+        <h3>About Us</h3>
+        	<span>Our webpage is made for Women Society. Their legal problem's one-stop solution is our webpage 'AVYANNA'. We are trying to help the Women Society to build a greater INDIA.</span><br><br><hr>
+             <!--  <h3><i class="fa fa-building" aria-hidden="true"></i>9A, Tarak Dutta Road, P.O Ballygunge, Kolkata, West Bengal</h3> -->
+           <p><sup><i class="fa fa-quote-left " aria-hidden="true"></i></sup>&nbsp;DON'T TREAT WOMEN AS AN OBJECT<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TREAT THEM AS A CO-CREATOR.&nbsp;<sup><i class="fa fa-quote-right fa-1x" aria-hidden="true"></i></sup></p><hr><br>
+			
+            <h3>Our Team</h3>
+
+			<a href="Team.html">Join Us</a>
+        </div>
+    </div>
 </div>
-<table>
-<tr>
-<td><img src="assets/newfinallogo.png" style="width:500px">
-</td>
-<td style="font-size:30px;"><fieldset style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; background-color:#000;"><font color="#fef773">Our webpage is made for Women Society.Their problem's one-step solution is our webpage 'AVYANNA'.Our well decorated webpage will help them to understand the laws regarding Women Society and they can take any legal step.We are trying to help the Women Society to build a greater INDIA.</font></fieldset></td> 
-<tr>
-</table><br><br>
-<center><fieldset style="font-size:50px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; "><legend style="width:50px;">"&nbsp;"</legend>"<font color="#000000">DON'T &nbsp;TREAT&nbsp; WOMEN&nbsp; AS&nbsp; AN &nbsp;OBJECT</font><br>
-&nbsp;&nbsp;<font color="#000000">TREAT &nbsp;THEM &nbsp;AS &nbsp; A  &nbsp; CO-CREATOR.</font>"</fieldset></center><br><br>
-
-<h1 align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:75px;">OUR TEAM</h1>
-
-<div class="row">
-  <div class="column">
-    <div class="card">
-    <img src="assets/Titisha.jpg" style="width:100%">
-      <h3 style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:20px;">Titisha Kuila</h3>
-      <p style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:18px;">BCA Student</p>
-      
-    </div>
-  </div>
-
-  
-  <div class="column">
-    <div class="card">
-    <img src="assets/Avijoy.jpg" style="width:96%">
-      <h3 style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:20px;">Avijoy Das Bhowmick</h3>
-      <p style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:18px;">BCA Student</p>
-      
-    </div>
-  </div>
-  <div class="column">
-    <div class="card">
-    <img src="assets/RiyaM.jpg" style="width:90%">
-      <h3 style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:20px;">Riya Sil</h3>
-      <p style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:18px;">Mentor</p>
-      
-    </div>
-  </div>
-  
-   <div class="column">
-    <div class="card">
-    <img src="assets/ayan.jpg" style="width:96%">
-      <h3 style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:20px;">Ayan Sikdar</h3>
-      <p style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:18px;">BCA Student</p>
-      
-    </div>
-  </div>
-  
-  
-   <div class="column">
-    <div class="card">
-    <img src="assets/Tariq.jpg" style="width:99%">
-      <h3 style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:20px;">Sardar Tariq Aziz</h3>
-      <p style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size:18px;">BCA Student</p>
-      
-    </div>
-  </div>
-</div><br><br>
-
+</div>
+</div>
+</section>
 </body>
 </html>
