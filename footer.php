8c0e26287df5ee884fe52c36c022e62d34a05017
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-04-08 20:41:49 +0530

    footer v1

--- footer.php
+++ footer.php
@@ -1,4 +1,78 @@
-<div>
-    <h2>This is footer</h2>
-    <h3>Footer section coming soon</h3>
-</div>
+
+<!-- Footer -->
+<footer class="page-footer font-small amber darken-2 footer">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase " style="color:#ffffff">Lab On IT</h5>
+        <p style="color:#ffffff">Test Today, To be Safe Tomorrow</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 style="color:#ffffff" class="text-uppercase">Legal Documents</h5>
+
+        <ul style="color:#ffffff" class="list-unstyled">
+          <li>
+            <a href="<?php echo base_url(); ?>index.php/welcome/terms" style="color:white">Terms and Condition</a>
+          </li>
+          <li>
+            <a href="<?php echo base_url(); ?>index.php/welcome/privacypolicy" style="color:white">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="<?php echo base_url(); ?>index.php/welcome/disclaimer" style="color:white">Disclaimer</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 style="color:#ffffff" class="text-uppercase">Links</h5>
+
+        <ul style="color:#ffffff" class="list-unstyled">
+          <li>
+            <a href="<?php echo base_url(); ?>index.php/welcome/about" style="color:white">About Us</a>
+          </li>
+          <li>
+            <a href="<?php echo base_url(); ?>index.php/welcome/faq" style="color:white">FAQ</a>
+          </li>
+          <li>
+            <a href="https://www.google.com/maps/place/Adamas+University/@22.7383075,88.454424,17z/data=!3m1!4b1!4m5!3m4!1s0x39f898ec702e5daf:0xfa41b06da8478350!8m2!3d22.7383026!4d88.4566127" style="color:white">Site Map</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div style="color:#ffffff" class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a style="color:#ffffff" href="<?php echo base_url(); ?>"> LabOnIt.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
