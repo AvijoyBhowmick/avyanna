23f94295ff687aecc38f759c37f8991ecf6fd19a
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-04-09 13:43:20 +0530

    final cover page

--- index.php
+++ index.php
@@ -1,35 +1,16 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
-	<title>Home Page</title>
-
-	 <!--Bootstrap CDNs-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<link rel="stylesheet" type="text/css" href="style.js">
-<link rel="stylesheet" type="text/css" href="style.css">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Arvo:wght@700&display=swap" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:wght@500&display=swap" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-
+<meta charset="utf-8">
+<title>Cover Page</title>
 <link rel="stylesheet" href="CSS/Cover.css">
 </head>
+
 <body>
-	
 <div class="god">
 	<h2>AVYANNA</h2><br><br><br><br><br><br>
     <p>"A Step Towards Women Rights"</p><br><br><br><br>
-    <a href="dashboard.php">GET STARTED</a>
+    <a href="#">GET STARTED</a>
 </div>
-
 </body>
 </html>
-
-		
-		
