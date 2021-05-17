77b6bb47efd9f6283fd053f60f05ea9b81543016
Avijoy Das Bhowmick <avijoydasbhowmick55@gmail.com>
2021-05-17 20:21:07 +0530

    blog v1.2

--- header.php
+++ header.php
@@ -5,6 +5,6 @@
      <a style="color: white; font-size: 15px; "class="nav-link" href="dashboard.php"><b>Home</b></a>
      <a style="color: white; font-size: 15px; "class="nav-link" href="category.php"><b>Category</b></a>
      <a style="color: white; font-size: 15px; "class="nav-link" href="aboutus.php"><b>About Us</b></a>
-     <a style="color: white; font-size: 15px; "class="nav-link" href="blogs.php"><b>Blogs </b></a>
+     <a style="color: white; font-size: 15px; "class="nav-link" href="Blog.html"><b>Blogs </b></a>
     
 </nav>
--- home.php
+++ home.php
@@ -36,6 +36,9 @@
             <a class="nav-link" href="Category.html">Category</a>
           </li>
           <li class="nav-item">
+            <a class="nav-link" href="Blog.html">Blogs</a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link" href="aboutus.php">About Us</a>
           </li>
         </ul>
